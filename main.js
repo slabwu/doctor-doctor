98668d62e0c6bbf782b054af643fa44dd5495b6a
slabwu <134193743+slabwu@users.noreply.github.com>
2025-04-30 05:11:47 +0800

    Add organ assets

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 import { Organ } from './organ.js';
 
-[]
+const UNIT = 120;
 
-for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-        new Organ('heart', i * 120 + 20, j * 120 + 20);
-    }
+let organs = [
+    ['heart', 0, 0],
+    ['brain', UNIT, 0],
+    ['intestines', 0, UNIT],
+    ['liver', 2 * UNIT, 0],
+    ['lung', 2 * UNIT, UNIT],
+    ['stomach', 3 * UNIT, 0],
+]
+
+for (let organ of organs) {
+    new Organ(organ[0], 20 + organ[1], 20 + organ[2]);
 }
+
+// for (let i = 0; i < 3; i++) {
+//     for (let j = 0; j < 3; j++) {
+//         new Organ('heart', i * 120 + 20, j * 120 + 20);
+//     }
+// }
